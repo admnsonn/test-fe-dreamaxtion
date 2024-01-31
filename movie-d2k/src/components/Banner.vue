49c33a8e155de1b549d733fadb351b1081b65ba2
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-01-31 22:12:47 +0700

    responsif

--- movie-d2k/src/components/Banner.vue
+++ movie-d2k/src/components/Banner.vue
@@ -15,11 +15,13 @@ const description = overview.length <= 200 ? overview : overview.slice(0, 200)
 <template>
     <div class="w-screen h-screen" :style="{
         backgroundImage: 'url(' + getImage(background) + ')',
-        backgroundPosition: 'center center'
+        backgroundPosition: 'center center',
+        backgroundAttachment: 'fixed'
     }">
         <div
             class="text-white p-10 flex flex-col justify-center w-full h-full bg-gradient-to-r from-black to to-transparent">
             <h1 class="text-8xl font-bold">{{ title }}</h1>
+            <br>
             <p class="mt-2 w-1/2 text-sm text-neutral-400">{{ description }}</p>
             <RouterLink :to="`/movies/${id}`"
                 class="flex items-center gap-1 px-6 py-4 rounded-md bg-blue-900 w-fit mt-4 transition hover:bg-neutral-800 font-semibold">
